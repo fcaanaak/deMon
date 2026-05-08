2c995d08906448b83f4e6f02082e04264a878d4a
fcanak <fcanak05@gmail.com>
2026-05-08 12:05:50 -0700

    Added basic behavior for adding reports

--- server/src/main/java/com/modesec/server/models/Report.java
+++ server/src/main/java/com/modesec/server/models/Report.java
@@ -2,10 +2,14 @@ package com.modesec.server.models;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 @Entity
 public class Report {
@@ -14,22 +18,25 @@ public class Report {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
+    @NotNull
     private String deviceName;
+
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Past
+    @NotNull
+    private LocalDateTime detectionDateTime;
 
     public Report(){}
 
-    public Report(String deviceName){
+    public Report(String deviceName, LocalDateTime detectionDateTime){
+        this.detectionDateTime = detectionDateTime;
         this.deviceName = deviceName;
     }
-
-
-//    private LocalDateTime detectionDateTime;
 
     public String getDeviceName() {
         return deviceName;
     }
+    public LocalDateTime getDetectionDateTime() {return detectionDateTime;}
 
-//    public LocalDateTime getDetectionDateTime() {
-//        return detectionDateTime;
-//    }
 }
--- server/src/main/java/com/modesec/server/services/ReportServiceImpl.java
+++ server/src/main/java/com/modesec/server/services/ReportServiceImpl.java
@@ -6,14 +6,32 @@ import com.modesec.server.repositories.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.modesec.server.websocket.handler.ReportUpdatesHandler;
+
+import java.io.IOException;
+
 @Service
 public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private ReportRepository reportRepository;
 
+    @Autowired
+    private ReportUpdatesHandler reportUpdatesHandler;
+
+    private void broadcastReport(Report report) {
+        try {
+            reportUpdatesHandler.broadcastReportToClients(report);
+        } catch (IOException e) {
+            // Do nothing for now
+        }
+    }
+
     @Override
     public void addReport(Report report) {
         reportRepository.save(report);
+        broadcastReport(report);
+
     }
+
 }
